54b1085919afaff03a8e351f29f34d05eda69edc
Louis <louis.zawadzki@ecl2015.ec-lyon.fr>
2016-01-08 20:35:51 +0100

    tweets in console

---
+++ app.js
@@ -0,0 +1,40 @@
+var express  = require('express'),
+    http     = require('http'),
+    socketio = require('socket.io'),
+    twitter  = require('twitter'),
+    twitterk = require('./twitter-key');
+
+var app = express();
+var port = 8001;
+
+// app
+app.use('/static', express.static('public'));
+app.get('/', function(req,res) {
+  res.sendFile("index.html", { root: __dirname + "/public" });
+});
+var server = http.createServer(app).listen(port, function() {
+  console.log("yippie");
+});
+
+// twitter
+var client = new twitter({
+  consumer_key: twitterk.key,
+  consumer_secret: twitterk.secret,
+  access_token_key: twitterk.token,
+  access_token_secret: twitterk.tsecret
+});
+
+var params = {track: 'vtecl'};
+client.stream('statuses/filter', params, function(stream){
+  stream.on('data', function(tweet) {
+    console.log(tweet.text);
+  });
+
+  stream.on('error', function(error) {
+    console.log(error);
+  });
+});
+
+
+//socket.io
+var io = socketio.listen(server);
